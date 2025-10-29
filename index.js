10d0a67f3484201301b9dd2cab78f694cbf61b70
Gabriel Reyes <greyes@quaxar.com>
2025-10-29 13:28:22 -0600

    feat: Implement GET route for homepage

--- index.js
+++ index.js
@@ -7,8 +7,25 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = '';
+
+app.get('/', async (req, res) => {
+    const concertsEndpoint = 'https://api.hubspot.com/crm/v3/objects/2-52193574?properties=concert_name,artist,event_date';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const resp = await axios.get(concertsEndpoint, { headers });
+        const data = resp.data.results;
+        
+        res.render('homepage', { title: 'Homepage | HubSpot APIs', data });
+
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 app.get('/update-cobj', (req, res) => {
     res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
